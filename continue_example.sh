f197cdae04a44dad8721ed5fde81a3d55a472caa
sameer jain <92518021+Coder-sam-007@users.noreply.github.com>
2023-12-13 21:02:50 +0530

    error fixed

--- continue_example.sh
+++ continue_example.sh
@@ -6,11 +6,11 @@ sleep 1
 echo "Going up"
 sleep 1
 
-for x in (1..17);
+for x in {10..17};
 
 do
 
-if [[ $x 13 ]]; then
+if [[ $x == 13 ]]; then
 continue
 
 fi
--- for_weather.sh
+++ for_weather.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 for city in $(cat cities.txt);
-
 do 
    weather=$(curl -s http://wttr.in/$city?format=3)
    echo "The weather for $weather"
